614461996aabcf2a3f1facb29fb077e3b4295bbd
mowuj <mowuj92@gmail.com>
2022-05-26 00:54:38 +0600

    use react router and set a navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Home/Blogs/Blogs';
+import Home from './Pages/Home/Home/Home';
+import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element= {<Home></Home>}></Route>
+        <Route path='blogs' element= {<Blogs></Blogs>}></Route>
+      </Routes>
     </div>
   );
 }
